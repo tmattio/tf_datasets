abd1382a57a8e1a2ee8df517ada39241660f247c
Thibaut Mattio <thibaut.mattio@gmail.com>
2017-08-15 17:42:36 +0800

    Add ciraf100 dataset

--- tf_datasets/__init__.py
+++ tf_datasets/__init__.py
@@ -7,6 +7,7 @@ __version__ = '0.1.0'
 
 from tf_datasets.datasets.mnist import mnist
 from tf_datasets.datasets.ciraf10 import ciraf10
+from tf_datasets.datasets.ciraf100 import ciraf100
 from tf_datasets.datasets.flowers import flowers
 from tf_datasets.datasets.fddb import fddb
 from tf_datasets.datasets.wider_face import wider_face
@@ -14,6 +15,7 @@ from tf_datasets.datasets.wider_face import wider_face
 _dataset_name_map = {
     'mnist': mnist,
     'ciraf10': ciraf10,
+    'ciraf100': ciraf100,
     'flowers': flowers,
     'fddb': fddb,
     'wider_face': wider_face,
